f9a0c76a8afad00e010c38080446deb272d2dc7d
TrungTQ <taquangtrungvn@yahoo.com>
2010-03-22 06:12:24 +0700

    delete execuatable file

--- reduce_project.sh
+++ reduce_project.sh
@@ -2,3 +2,9 @@ find -name *.o -delete
 find -name *.d -delete
 rm ./SLR-spass/Debug/SLR
 rm ./SLR-spass/Debug/SLR-spass
+rm ./SLR-spass/SPASS/dfg2ascii
+rm ./SLR-spass/SPASS/dfg2dfg
+rm ./SLR-spass/SPASS/dfg2otter
+rm ./SLR-spass/SPASS/dfg2tptp
+rm ./SLR-spass/SPASS/SPASS
+
